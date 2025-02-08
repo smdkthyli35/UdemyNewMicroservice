96e9a006f56127e65ee13eceb740f697aec82bbd
Samed Kütahyalı <smdkthyli@gmail.com>
2025-02-08 17:17:28 +0300

    feat: Add db context and entity configurations for course and category

---
+++ UdemyNewMicroservice.Catalog.Api/Repositories/CategoryEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MongoDB.EntityFrameworkCore.Extensions;
+using UdemyNewMicroservice.Catalog.Api.Features.Categories;
+
+namespace UdemyNewMicroservice.Catalog.Api.Repositories
+{
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToCollection("categories");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.Name).HasElementName("name").HasMaxLength(100);
+            builder.Ignore(x => x.Courses);
+        }
+    }
+}
